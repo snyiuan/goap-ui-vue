4edc7d38519c55cafcaff72ced9eabe818d3264e
snyiuan <451120121@qq.com>
2021-01-04 21:21:00 +0800

    0.09

--- src/components/action-item/action-item.vue
+++ src/components/action-item/action-item.vue
@@ -1,93 +1,108 @@
 <template>
-  <a-card :title="item">
-    <a-row type="flex" justify="space-between">
-      <a-col>Name</a-col
-      ><a-col><a-input type="text" v-model="stateActions.arr[index]" /></a-col>
-    </a-row>
-    <a-row type="flex" justify="space-between">
-      <a-col>Cost</a-col><a-col><a-input type="number" v-model="cost" /></a-col>
-    </a-row>
-    <a-row type="flex" justify="space-between">
-      <a-col>Pre Conditions</a-col>
-      <a-col>
-        <a-dropdown :trigger="['click']">
-          <a-button
-            icon="plus-circle"
-            type="link"
-            shape="circle"
-            size="small"
-          ></a-button>
-          <a-menu slot="overlay">
-            <template v-for="(condition, index) in conditions.arr">
-              <a-menu-item :key="index">
-                <a @click.prevent="addPreCondition(index)">{{ condition }}</a>
-              </a-menu-item>
-            </template>
-          </a-menu>
-        </a-dropdown>
-      </a-col>
-    </a-row>
-    <a-list :data-source="preConditions" item-layout="vertical">
-      <a-list-item slot="renderItem" slot-scope="condition, index">
-        <a-row type="flex" justify="space-between">
-          <a-col
-            ><a-checkbox @chang="changeConditionState(condition)">{{
-              conditions.arr[condition]
-            }}</a-checkbox></a-col
-          >
-          <a-col
-            ><a-button
-              icon="minus-circle"
+  <a-list-item class="drag-div" style="position: relative" v-drag>
+    <a-card :title="item.name">
+      <a-input
+        addonBefore="Name"
+        type="text"
+        v-model="stateActions[index].name"
+      />
+      <br />
+      <br />
+      <a-input addonBefore="Cost" type="number" v-model="item.cost" />
+      <br />
+      <br />
+      <a-row type="flex" justify="space-between">
+        <a-col>Pre Conditions</a-col>
+        <a-col>
+          <a-dropdown :trigger="['hover']">
+            <a-button
+              icon="plus-circle"
               type="link"
               shape="circle"
               size="small"
-              @click="removePreCondition(index)"
-            ></a-button
-          ></a-col>
-        </a-row>
-      </a-list-item>
-    </a-list>
-    <a-row type="flex" justify="space-between">
-      <a-col>Post Conditions</a-col>
-      <a-col>
-        <a-dropdown :trigger="['click']">
-          <a-button
-            icon="plus-circle"
-            type="link"
-            shape="circle"
-            size="small"
-          ></a-button>
-          <a-menu slot="overlay">
-            <template v-for="(condition, index) in conditions.arr">
-              <a-menu-item :key="index">
-                <a @click.prevent="addPostCondition(index)">{{ condition }}</a>
-              </a-menu-item>
-            </template>
-          </a-menu>
-        </a-dropdown>
-      </a-col>
-    </a-row>
-    <a-list :data-source="postConditions" item-layout="vertical">
-      <a-list-item slot="renderItem" slot-scope="condition, index">
-        <a-row type="flex" justify="space-between">
-          <a-col
-            ><a-checkbox @chang="changeConditionState(condition)">{{
-              conditions.arr[index]
-            }}</a-checkbox></a-col
-          >
-          <a-col
-            ><a-button
-              icon="minus-circle"
+            ></a-button>
+            <a-menu slot="overlay">
+              <template v-for="(condition, conditionIndex) in conditions.arr">
+                <a-menu-item :key="conditionIndex">
+                  <a @click.prevent="addPreCondition(conditionIndex)">{{
+                    condition
+                  }}</a>
+                </a-menu-item>
+              </template>
+            </a-menu>
+          </a-dropdown>
+        </a-col>
+      </a-row>
+      <div class="pre-conditions" v-if="item.preConditions.length">
+        <a-list :data-source="item.preConditions" item-layout="vertical">
+          <a-list-item slot="renderItem" slot-scope="condition, index">
+            <a-row type="flex" justify="space-between">
+              <a-col
+                ><a-checkbox v-model="condition.state">{{
+                  conditions.arr[condition.conditionIndex]
+                }}</a-checkbox></a-col
+              >
+              <a-col
+                ><a-button
+                  icon="minus-circle"
+                  type="link"
+                  shape="circle"
+                  size="small"
+                  @click="removePreCondition(index)"
+                ></a-button
+              ></a-col>
+            </a-row>
+          </a-list-item>
+        </a-list>
+      </div>
+
+      <a-row type="flex" justify="space-between">
+        <a-col>Post Conditions</a-col>
+        <a-col>
+          <a-dropdown :trigger="['hover']">
+            <a-button
+              icon="plus-circle"
               type="link"
               shape="circle"
               size="small"
-              @click="removePostCondition(index)"
-            ></a-button
-          ></a-col>
-        </a-row>
-      </a-list-item>
-    </a-list>
-  </a-card>
+            ></a-button>
+            <a-menu slot="overlay">
+              <template v-for="(condition, conditionIndex) in conditions.arr">
+                <a-menu-item :key="conditionIndex">
+                  <a @click.prevent="addPostCondition(conditionIndex)">{{
+                    condition
+                  }}</a>
+                </a-menu-item>
+              </template>
+            </a-menu>
+          </a-dropdown>
+        </a-col>
+      </a-row>
+
+      <div class="post-conditions" v-if="item.postConditions.length">
+        <a-list :data-source="item.postConditions" item-layout="vertical">
+          <a-list-item slot="renderItem" slot-scope="condition, index">
+            <a-row type="flex" justify="space-between">
+              <a-col
+                ><a-checkbox v-model="condition.state">{{
+                  conditions.arr[condition.conditionIndex]
+                }}</a-checkbox></a-col
+              >
+              <a-col
+                ><a-button
+                  icon="minus-circle"
+                  type="link"
+                  shape="circle"
+                  size="small"
+                  @click="removePostCondition(index)"
+                ></a-button
+              ></a-col>
+            </a-row>
+          </a-list-item>
+        </a-list>
+      </div>
+    </a-card>
+  </a-list-item>
 </template>
 
 <script>
@@ -95,41 +110,27 @@ import { mapState } from "vuex";
 export default {
   props: ["item", "index"],
   methods: {
-    addPreCondition(index) {
-      if (this.preConditions.indexOf(index) === -1) {
-        this.preConditions.push(index);
-      } else {
-        this.$message.info("PreConditions are repeated");
-      }
-    },
-    addPostCondition(index) {
-      if (this.postConditions.indexOf(index) === -1) {
-        this.postConditions.push(index);
-      } else {
-        this.$message.info("PostConditions are repeated");
-      }
+    addPreCondition(conditionIndex) {
+      console.log("addPre");
+      this.$store.dispatch("addPreCondition", {
+        name: this.item.name,
+        index: conditionIndex,
+        vue: this,
+      });
     },
     removePreCondition(index) {
-      this.preConditions.splice(index, 1);
+      this.item.preConditions.splice(index, 1);
+    },
+    addPostCondition(conditionIndex) {
+      console.log("addPost");
+      this.$store.dispatch("addPostCondition", {
+        name: this.item.name,
+        index: conditionIndex,
+        vue: this,
+      });
     },
     removePostCondition(index) {
-      this.postConditions.splice(index, 1);
-    },
-    changeConditionState(condition) {
-      console.log(condition);
-    },
-  },
-  data() {
-    return {
-      cost: 0,
-      preConditions: [],
-      postConditions: [],
-    };
-  },
-  watch: {
-    cost(val) {
-      console.log(val);
-      // this.conditions.arr[this.index].cost
+      this.item.postConditions.splice(index, 1);
     },
   },
   computed: {
